c0e8142ef9ad6e508a44cbcc8de0c50ee2a53502
egonzalezqw <egofac@gmail.com>
2026-05-15 13:48:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,8 @@
-st.markdown("""
-<style>
-.stApp {
-    background-color: #0E1117;
-    color: white;
-}
+import streamlit as st
+import time
 
-.block-container {
-    padding-top: 2rem;
-    color: white;
-}
-
-h1, h2, h3, h4, p, label, span {
-    color: white !important;
-}
-
-.stButton>button {
-    width: 100%;
-    border-radius: 10px;
-    height: 3em;
-    font-size: 16px;
-    font-weight: bold;
-}
-
-.score-box {
-    padding: 20px;
-    border-radius: 12px;
-    background-color: #1E293B;
-    text-align: center;
-    margin-bottom: 20px;
-    color: white;
-}
-
-.level-title {
-    color: #38BDF8;
-}
-</style>
-""", unsafe_allow_html=True)
+st.set_page_config(
+    page_title="Network Defender Challenge",
+    page_icon="🛡️",
+    layout="wide"
+)
